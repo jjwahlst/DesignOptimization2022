f931c7e5ada011c1b659cf78685708579b20ff81
jjwahlst <jjwahlst@asu.edu>
2022-10-12 22:55:59 -0700

    I missed a **2

--- Homework_3.py
+++ Homework_3.py
@@ -16,10 +16,13 @@ X1 = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
 X2 = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0])
 
 a = np.array([[8.07131, 1730.63, 233.426], [7.43155, 1554.679, 240.337]])
+# print(a)
+
+
 T = 20
 
-p1sat = 10 ** (a[0, 0] - a[0, 1] / (20 + a[0, 2]))
-p2sat = 10 ** (a[1, 0] - a[1, 1] / (20 + a[1, 2]))
+p1sat = 10 ** (a[0, 0] - (a[0, 1] / (T + a[0, 2])))
+p2sat = 10 ** (a[1, 0] - (a[1, 1] / (T + a[1, 2])))
 print(p1sat)
 print(p2sat)
 P = np.array([28.1, 34.4, 36.7, 36.9, 36.8, 36.7, 36.5, 35.4, 32.9, 27.7, 17.5])
@@ -29,13 +32,15 @@ A = Variable(torch.tensor([1.0, 1.0]), requires_grad=True)
 
 x1 = torch.tensor(X1, requires_grad=False, dtype=torch.float32)
 x2 = torch.tensor(X2, requires_grad=False, dtype=torch.float32)
-print(x1)
-print(x2)
+# print(x1)
+# print(x2)
+
 
 a = 0.0001
 
 for i in range(1000):
-    P_pred = x1 * torch.exp(A[0] * ((A[1] * x2) / (A[0] * x1 + A[1] * x2)) ** 2) * p1sat + x2 * torch.exp(A[1] * ((A[1] * x1) / (A[0] * x1 + A[1] * x2))) * p2sat
+    P_pred = x1 * torch.exp(A[0] * ((A[1] * x2) / (A[0] * x1 + A[1] * x2)) ** 2) * p1sat + x2 * torch.exp(A[1] * \
+            ((A[1] * x1) / (A[0] * x1 + A[1] * x2)) ** 2) * p2sat
     loss = (P_pred - P) ** 2
     loss = loss.sum()
     loss.backward()
