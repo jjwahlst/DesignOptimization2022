59571764dc14c0728a8756c644ee9eaf49f34a07
jjwahlst <jjwahlst@asu.edu>
2022-10-12 21:29:53 -0700

    Updated problem 1 with notes from class

--- Homework_3.py
+++ Homework_3.py
@@ -5,21 +5,50 @@ from scipy.optimize import minimize
 import math
 import bayes_opt
 from bayes_opt import BayesianOptimization
+import torch
+from torch.autograd import Variable
 import warnings
 
-
 # Problem 1
-a11 = 8.07131
-a12 = 1730.63
-a13 = 233.426
 
-p1sat = 10 ** (a11 - a12/(20+a13))
 
-a21 = 7.43155
-a22 = 1554.679
-a23 = 240.337
+X1 = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+X2 = np.flip(x1, axis=1).copy()
 
-p2sat = 10 ** (a21 - a22/(20+a23))
+a = np.array([[8.07131, 1730.63, 233.426], [7.43155, 1554.679, 240.337]])
+T = 20
+
+p1sat = 10 ** (a[0, 0] - a[0, 1] / (20 + a[0, 2]))
+p2sat = 10 ** (a[1, 0] - a[1, 1] / (20 + a[1, 2]))
+P = np.array([28.1, 34.4, 36.7, 36.9, 36.8, 36.7, 36.5, 35.4, 32.9, 27.7, 17.5])
+P = torch.tensor(P, requires_grad=False, dtype=torch.float32)
+
+A = Variable(torch.tensor([1.0, 1.0]), requires_grad=True)
+
+x1 = torch.tensor(X1, requires_grad=False, dtype=torch.float32)
+x2 = torch.tensor(X2, requires_grad=False, dtype=torch.float32)
+
+a = 0.0001
+
+for i in range(100):
+    P_pred = x1 * torch.exp(A[0] * ((A[1] * x2) / (A[0] * x1 + A[1] * x2)) ** 2) * p1sat + x2 * torch.exp(
+        A[1] * ((A[1] * x2) / (A[0] * x1 + A[1] * x2))) * p2sat
+    loss = (P_pred - P) ** 2
+    loss = loss.sum()
+
+    loss.backward()
+
+    with torch.no_grad():
+        A -= a * A.grad()
+
+        A.grad.zero()
+
+print('Esimation A12, A21 is:', A)
+print('FInal loss is:', loss.data.numpy())
+
+
+
+
 # CHECK
 # print('P1sat', p1sat, 'P2sat', p2sat)
 
